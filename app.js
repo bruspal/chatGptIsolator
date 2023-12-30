83d58b77498ab5178dc40c54386dcc7c84eaac54
bruspal <bruspal@bruspal.net>
2023-12-30 15:08:14 +0400

    Adding app icon and check for maximum context isolation + comments and typos

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
 const { app, BrowserWindow } = require('electron')
 
+// Create global reference to mainWindow
 let mainWindow
 
+/*
+ * Create the main window
+ */
 function createWindow() {
-    const browserOptions = {
+    const iconPath = __dirname + '/assets/logo/GPTIsolator_32x32.png';
+    const BrowserWindowOptions = {
         width: 1600,
         height: 900,
-        icon: __dirname + 'assets/logo/GPTIsolator_16x16.ico',
+        icon: iconPath,
         webPreferences: {
-            /*
-             * security : disabling unsafe-eval #WIP
-             */
-            // Disable node in the browser context
-            nodeIntegration: false,
-            // set contextIsolation to false for a quick test.
-            // TODO: implement preloading in order to disable contextIsolation
-            contextIsolation: false,
-            // Disable sandboxing
-            sandbox: false,
-            // prevent ujnsafe JS evaluation on unsecure context
-            worldSafeExecuteJavaScript: true,
-            // Disable remote module
-            enableRemoteModule: false,
+            nodeIntegration: false,             // Disable node in the browser context (false by default)
+            contextIsolation: true,             // set contextIsolation to protect against prototype pollution
+            sandbox: true,                      // set sandboxing to true, useless in this context since app.enableSandbox() is called later. This is just for educational purpose.
+            worldSafeExecuteJavaScript: true,   // prevent unsafe JS evaluation on unsecure context
+            enableRemoteModule: false,          // Disable remote module
         },
     }
-    console.log(browserOptions)
-    mainWindow = new BrowserWindow(browserOptions)
+    mainWindow = new BrowserWindow(BrowserWindowOptions)
 
     // Charger le site OpenAI Chat
     mainWindow.loadURL('https://chat.openai.com')
 
+    // mainWindow.webContents.openDevTools();
     mainWindow.on('closed', function () {
         mainWindow = null
     })
 }
 
-app.whenReady().then(createWindow)
+// Enable sandboxing application wise
+app.enableSandbox()
 
+// Create the main window when electron is ready
+app.whenReady().then(() => {
+    createWindow()
+    // If application is running but no window is open create a new instance of the main window.
+    app.on('activate', function () {
+        if (mainWindow === null) createWindow()
+    })
+})
+
+// Quit when all windows are closed.
 app.on('window-all-closed', function () {
+    // On macOS it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', function () {
-    if (mainWindow === null) createWindow()
-})
